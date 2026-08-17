4f8c64f510ba44494cb358c6558bdcf1f9e3d66d
Luis <luisnurenagonzales@gmail.com>
2026-08-17 17:57:43 -0500

    Configure player_skip=configs and web_embedded,android bypass

--- server.js
+++ server.js
@@ -247,7 +247,7 @@ app.post('/api/info', async (req, res) => {
         noPlaylist: true,
         noWarnings: true,
         noCheckCertificates: true,
-        extractorArgs: 'youtube:player_client=tv_embedded,android_music,web_embedded,mweb',
+        extractorArgs: 'youtube:player_skip=configs;player_client=web_embedded,android',
       });
 
       const durationSec = Number(info.duration) || 0;
@@ -468,7 +468,7 @@ app.post('/api/download', async (req, res) => {
       noCheckCertificates: true,
       ffmpegLocation: ffmpeg.path,
       output: outputTemplate,
-      extractorArgs: 'youtube:player_client=tv_embedded,android_music,web_embedded,mweb',
+      extractorArgs: 'youtube:player_skip=configs;player_client=web_embedded,android',
     };
 
     let postArgs = [];
@@ -491,7 +491,7 @@ app.post('/api/download', async (req, res) => {
         formatSelector = '18/134+140/bestvideo[height<=360]+bestaudio/best[height<=360]/best';
       }
 
-      ytOptions.extractorArgs = 'youtube:player_client=tv_embedded,android_music,web_embedded,mweb';
+      ytOptions.extractorArgs = 'youtube:player_skip=configs;player_client=web_embedded,android';
       ytOptions.format = formatSelector;
       ytOptions.mergeOutputFormat = 'mp4';
       ytOptions.windowsFilenames = true;
@@ -502,7 +502,7 @@ app.post('/api/download', async (req, res) => {
     } else {
       ytOptions.format = '251/140/bestaudio/best';
       ytOptions.extractAudio = true;
-      ytOptions.extractorArgs = 'youtube:player_client=tv_embedded,android_music,web_embedded,mweb';
+      ytOptions.extractorArgs = 'youtube:player_skip=configs;player_client=web_embedded,android';
       ytOptions.concurrentFragments = 4;
       ytOptions.windowsFilenames = true;
 
